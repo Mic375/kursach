57ac4715089441f39afab8b9ec021604b588a080
Mic375 <miha.maksimov.071102@gmail.com>
2020-12-01 15:48:15 +0700

    update

--- Курсач/Form1.Designer.cs
+++ Курсач/Form1.Designer.cs
@@ -240,7 +240,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(668, 473);
+            this.ClientSize = new System.Drawing.Size(668, 450);
             this.ControlBox = false;
             this.Controls.Add(this.label4);
             this.Controls.Add(this.Bx_ic);
